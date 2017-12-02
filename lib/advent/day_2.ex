c77ad460a966fb7a95ca681de205e00989042433
Rebecca Skinner <traybaby@gmail.com>
2017-12-02 16:06:08 +0800

    Day 2 part 2

--- lib/advent/day_2.ex
+++ lib/advent/day_2.ex
@@ -1,12 +1,22 @@
 defmodule Advent.Day2 do
   def part1(input) do
     input
+    |> String.trim
     |> String.split("\n")
     |> Enum.map(&to_ints/1)
     |> Enum.map(&part_1_line_value/1)
     |> Enum.sum
   end
-  
+
+  def part2(input) do
+    input
+    |> String.trim
+    |> String.split("\n")
+    |> Enum.map(&to_ints/1)
+    |> Enum.map(&part_2_line_value/1)
+    |> Enum.sum
+  end
+
   defp to_ints(line) do
     line
     |> String.split
@@ -15,5 +25,18 @@ defmodule Advent.Day2 do
 
   defp part_1_line_value(line) do
     Enum.max(line) - Enum.min(line)
+  end
+
+  defp part_2_line_value(line) do
+    line
+    |> Enum.sort
+    |> find_divisible([])
+  end
+
+  defp find_divisible([x | xs], ys) do
+    case Enum.find(ys, fn y -> rem(x, y) == 0.0 end) do
+      nil -> find_divisible(xs, [x | ys])
+      y -> div(x, y)
+    end
   end
 end
--- test/day_2_test.exs
+++ test/day_2_test.exs
@@ -6,4 +6,8 @@ defmodule Advent.Day2Test do
   test "example 1" do
     assert Day2.part1("5 1 9 5\n7 5 3\n2 4 6 8") == 18
   end
+
+  test "example 2" do
+    assert Day2.part2("5 9 2 8\n9 4 7 3\n3 8 6 5") == 9
+  end
 end
