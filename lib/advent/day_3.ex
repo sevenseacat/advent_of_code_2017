a17a5a17230fa2f99237c5bf0c48251acbf26894
Rebecca Skinner <traybaby@gmail.com>
2017-12-05 23:32:03 +0800

    Day 3 part 2

--- lib/advent/day_3.ex
+++ lib/advent/day_3.ex
@@ -1,5 +1,5 @@
 defmodule Advent.Day3.Coordinate do
-  defstruct x: 0, y: 0, value: 0
+  defstruct x: 0, y: 0, value: 1
 end
 
 defmodule Advent.Day3.Progress do
@@ -22,19 +22,24 @@ defmodule Advent.Day3.Progress do
   # run 6: 3 moves up ... see where this is going?
   # So a progress state is { run number, direction, # of moves in run, current move # in run }
   def new do
-    {1, {1,0}, 1, 1}
+    {1, {1,0}, 1, 0}
   end
 
-  def increment(progress, coordinates, num) do
+  def increment(progress, coordinates, fun) do
     new_progress = make_move(progress)
-    {x, y} = move(progress, coordinates)
-    { new_progress, %Coordinate{x: x, y: y, value: num} }
+    {x, y} = move(new_progress, coordinates)
+    { new_progress, %Coordinate{x: x, y: y, value: fun.({x, y}, coordinates)} }
   end
 
   defp make_move({run_no, direction, move_count, move_no}) do
     case move_count == move_no do
-      true  -> { run_no+1, rotate(direction), div(run_no, move_count), 1 }
-      false -> { run_no,   direction,         move_count,              move_no+1 }
+      true  ->
+        new_move_count = case rem(run_no, 2) do
+          0 -> move_count + 1
+          1 -> move_count
+        end
+        { run_no+1, rotate(direction), new_move_count, 1 }
+      false -> { run_no,   direction,  move_count,     move_no+1 }
     end
   end
 
@@ -48,14 +53,14 @@ defmodule Advent.Day3.Progress do
 end
 
 defmodule Advent.Day3.Board do
-  alias Advent.Day3.Progress
+  alias Advent.Day3.{Coordinate, Progress}
 
-  def build(input), do: do_build(Enum.into(1..input, []), [], Progress.new)
+  def build(input, fun), do: do_build(Enum.into(1..input, []), [%Coordinate{}], Progress.new, fun)
 
-  defp do_build([_num], coordinates, _), do: coordinates
-  defp do_build([num | nums], coordinates, progress) do
-    { new_progress, new_coordinate } = Progress.increment(progress, coordinates, num)
-    do_build(nums, [new_coordinate | coordinates], new_progress)
+  defp do_build([_num], coordinates, _, _), do: coordinates
+  defp do_build([num | nums], coordinates, progress, fun) do
+    { new_progress, new_coordinate } = Progress.increment(progress, coordinates, fun.(num))
+    do_build(nums, [new_coordinate | coordinates], new_progress, fun)
   end
 end
 
@@ -77,10 +82,71 @@ defmodule Advent.Day3 do
   """
   def part1(input) when is_integer(input) do
     input
-    |> Board.build
+    |> Board.build(fn num -> fn _, _ -> num+1 end end)
     |> calculate_distance
   end
 
-  defp calculate_distance([]), do: 0
+  @doc """
+  This contains a total hack, with exceptions as control flow.
+  The reason for this is that the input figure will try to calculate a board of that size, and
+  larger boards are exponentially larger to generate (due to the formula for calculating the
+  coordinate value).
+  We don't want a board of that size - we want to generate board data until the value exceeds the
+  input - and I don't know of any other way to break generation early. So here we go.
+
+  iex> Day3.part2(10)
+  %Coordinate{x: -1, y: -1, value: 11}
+
+  iex> Day3.part2(50)
+  %Coordinate{x: 2, y: 0, value: 54}
+
+  iex> Day3.part2(100)
+  %Coordinate{x: 1, y: 2, value: 122}
+
+  iex> Day3.part2(200)
+  %Coordinate{x: -2, y: 1, value: 304}
+  """
+  def part2(input) when is_integer(input) do
+    try do
+      input
+      |> Board.build(fn _ -> fn {x, y}=position, coords ->
+        val = calculate_coordinate_value(position, coords)
+        case val > input do
+          false -> val
+          true -> raise RuntimeError, message: %Coordinate{x: x, y: y, value: val}
+        end
+      end end)
+    rescue
+      e in RuntimeError -> e.message
+    end
+  end
+
+  @doc """
+  Given an {x,y} coordinate, determine the value for it based on all the calculated coordinates so far.
+  iex> Day3.calculate_coordinate_value({-1, 0}, [
+  ...>  %Coordinate{x: 0, y: 0, value: 1}, %Coordinate{x: 1, y: 0, value: 1}, %Coordinate{x: 1, y: 1, value: 2},
+  ...>  %Coordinate{x: 0, y: 1, value: 4}, %Coordinate{x: -1, y: 1, value: 5}])
+  10
+
+  iex> Day3.calculate_coordinate_value({0, -1}, [
+  ...>  %Coordinate{x: 0, y: 0, value: 1}, %Coordinate{x: 1, y: 0, value: 1}, %Coordinate{x: 1, y: 1, value: 2},
+  ...>  %Coordinate{x: 0, y: 1, value: 4}, %Coordinate{x: -1, y: 1, value: 5}, %Coordinate{x: -1, y: 0, value: 10},
+  ...>  %Coordinate{x: -1, y: -1, value: 11}])
+  23
+  """
+  def calculate_coordinate_value({x,y}, coords) do
+    # Each coordinate has eight possible values which must be summed up... but there will be a maximum of 4.
+    coords
+    |> find_coordinates([{x+1, y}, {x+1, y+1}, {x, y+1}, {x-1, y+1}, {x-1, y}, {x-1, y-1}, {x, y-1}, {x+1, y-1}])
+    |> Enum.map(&(&1.value))
+    |> Enum.sum
+  end
+
   defp calculate_distance([%Coordinate{x: x, y: y} | _]), do: abs(x) + abs(y)
+
+  defp find_coordinates(all_coords, to_find) do
+    all_coords
+    |> Stream.filter(fn coordinate -> Enum.member?(to_find, {coordinate.x, coordinate.y}) end)
+    |> Enum.take(4)
+  end
 end
--- test/day_3_test.exs
+++ test/day_3_test.exs
@@ -1,5 +1,6 @@
 defmodule Advent.Day3Test do
   use ExUnit.Case
   alias Advent.Day3
+  alias Advent.Day3.Coordinate
   doctest Advent.Day3
 end
