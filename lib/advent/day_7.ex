044c6f8514f52b2e724f6c08aa55c013b2b9677b
Rebecca Skinner <traybaby@gmail.com>
2017-12-12 22:52:25 +1100

    Day 7, part 2 - a lot of misfires to get here

--- lib/advent/day_7.ex
+++ lib/advent/day_7.ex
@@ -1,7 +1,7 @@
 defmodule Advent.Day7.Program do
   alias __MODULE__
 
-  defstruct name: nil, weight: 0, holding: []
+  defstruct name: nil, weight: 0, holding: [], stack_weight: 0
 
   def find(name, programs) do
     Enum.find(programs, &(&1.name == name))
@@ -9,6 +9,41 @@ defmodule Advent.Day7.Program do
 
   def held_by_noone?(program, programs) do
     !Enum.any?(programs, fn p -> Enum.member?(p.holding, program.name) end)
+  end
+
+  def unbalanced_child_of(program, programs) do
+    case balanced?(program, programs) do
+      true -> nil
+      false ->
+        children = program_refs(program.holding, programs)
+        Enum.find(children, &(odd_stack_weight(&1, children)))
+    end
+  end
+
+  # A program is balanced if all of the programs it directly holds have the same stack weight.
+  def balanced?(%Program{holding: []}, _), do: true
+  def balanced?(%Program{holding: holding}, programs) do
+    weights = program_refs(holding, programs)
+    |> Enum.map(&(Map.get(&1, :stack_weight)))
+    |> Enum.uniq
+
+    length(weights) == 1
+  end
+
+  def program_refs(names, programs) do
+    Enum.map(names, &(find(&1, programs)))
+  end
+
+  def siblings(program, programs) do
+    programs
+    |> Enum.find(&(Enum.member?(&1.holding, program.name)))
+    |> Map.get(:holding)
+    |> Enum.reject(&(&1 == program.name))
+    |> program_refs(programs)
+  end
+
+  defp odd_stack_weight(program, programs) do
+    Enum.count(programs, &(&1.stack_weight == program.stack_weight)) == 1
   end
 end
 
@@ -20,11 +55,30 @@ defmodule Advent.Day7 do
     |> Enum.find(&(Program.held_by_noone?(&1, programs)))
   end
 
+  def part2(programs) do
+    programs = Enum.map(programs, &(assign_stack_weight(&1, programs)))
+    root = part1(programs)
+
+    leaf = find_leaf(root, programs)
+    siblings = Program.siblings(leaf, programs)
+    calculate_difference(leaf, siblings)
+  end
+
+  defp find_leaf(unbalanced, programs) do
+    case Program.unbalanced_child_of(unbalanced, programs) do
+      nil -> unbalanced
+      child -> find_leaf(child, programs)
+    end
+  end
+
   def parse_input(input) do
-    input
+    programs = input
     |> String.trim
     |> String.split("\n")
     |> Enum.map(&convert_to_program/1)
+
+    programs
+    |> Enum.map(&(assign_stack_weight(&1, programs)))
   end
 
   defp convert_to_program(input) do
@@ -34,5 +88,25 @@ defmodule Advent.Day7 do
       weight: String.to_integer(data["weight"]),
       holding: String.split(data["holding"], ", ", trim: true)
     }
+  end
+
+  def assign_stack_weight(program, programs) do
+    %{program | stack_weight: calculate_stack_weight(program, programs)}
+  end
+
+  defp calculate_stack_weight(%Program{weight: weight, holding: []}, _), do: weight
+  defp calculate_stack_weight(%Program{weight: weight, holding: holding}, programs) do
+    weight + (holding
+    |> Stream.map(&(Program.find(&1, programs)))
+    |> Stream.map(&(calculate_stack_weight(&1, programs)))
+    |> Enum.sum)
+  end
+
+  defp calculate_difference(program, siblings) do
+    difference = siblings
+    |> Enum.map(&(&1.stack_weight - program.stack_weight))
+    |> Enum.find(&(&1 != 0))
+
+    {program, difference}
   end
 end
--- test/day_7_test.exs
+++ test/day_7_test.exs
@@ -5,19 +5,20 @@ defmodule Advent.Day7Test do
   doctest Advent.Day7
 
   setup_all do
-    programs = [%Program{name: "pbga", weight: 66, holding: []},
-                %Program{name: "xhth", weight: 57, holding: []},
-                %Program{name: "ebii", weight: 61, holding: []},
-                %Program{name: "havc", weight: 66, holding: []},
-                %Program{name: "ktlj", weight: 57, holding: []},
-                %Program{name: "fwft", weight: 72, holding: ["ktlj", "cntj", "xhth"]},
-                %Program{name: "qoyq", weight: 66, holding: []},
-                %Program{name: "padx", weight: 45, holding: ["pbga", "havc", "qoyq"]},
-                %Program{name: "tknk", weight: 41, holding: ["ugml", "padx", "fwft"]},
-                %Program{name: "jptl", weight: 61, holding: []},
-                %Program{name: "ugml", weight: 68, holding: ["gyxo", "ebii", "jptl"]},
-                %Program{name: "gyxo", weight: 61, holding: []},
-                %Program{name: "cntj", weight: 57, holding: []}]
+    # Stack weights calculated manually
+    programs = [%Program{name: "pbga", weight: 66, stack_weight: 66, holding: []},
+                %Program{name: "xhth", weight: 57, stack_weight: 57, holding: []},
+                %Program{name: "ebii", weight: 61, stack_weight: 61, holding: []},
+                %Program{name: "havc", weight: 66, stack_weight: 66, holding: []},
+                %Program{name: "ktlj", weight: 57, stack_weight: 57, holding: []},
+                %Program{name: "fwft", weight: 72, stack_weight: 243, holding: ["ktlj", "cntj", "xhth"]},
+                %Program{name: "qoyq", weight: 66, stack_weight: 66, holding: []},
+                %Program{name: "padx", weight: 45, stack_weight: 243, holding: ["pbga", "havc", "qoyq"]},
+                %Program{name: "tknk", weight: 41, stack_weight: 778, holding: ["ugml", "padx", "fwft"]},
+                %Program{name: "jptl", weight: 61, stack_weight: 61, holding: []},
+                %Program{name: "ugml", weight: 68, stack_weight: 251, holding: ["gyxo", "ebii", "jptl"]},
+                %Program{name: "gyxo", weight: 61, stack_weight: 61, holding: []},
+                %Program{name: "cntj", weight: 57, stack_weight: 57, holding: []}]
 
     [programs: programs]
   end
@@ -29,6 +30,18 @@ defmodule Advent.Day7Test do
 
   test "part 1", context do
     assert Day7.part1(context[:programs]) ==
-      %Program{name: "tknk", weight: 41, holding: ["ugml", "padx", "fwft"]}
+      %Program{name: "tknk", weight: 41, stack_weight: 778, holding: ["ugml", "padx", "fwft"]}
+  end
+
+  test "checking for balance", context do
+    assert Program.balanced?(Program.find("ugml", context[:programs]), context[:programs]) == true
+    assert Program.balanced?(Program.find("padx", context[:programs]), context[:programs]) == true
+    assert Program.balanced?(Program.find("gyxo", context[:programs]), context[:programs]) == true
+    assert Program.balanced?(Program.find("tknk", context[:programs]), context[:programs]) == false
+  end
+
+  test "part 2", context do
+    assert Day7.part2(context[:programs]) ==
+      {%Program{name: "ugml", weight: 68, stack_weight: 251, holding: ["gyxo", "ebii", "jptl"]}, -8}
   end
 end
