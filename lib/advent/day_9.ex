eb8c2a1aaf5a0e68a89447e2e48c3d1af7569d3b
Rebecca Skinner <traybaby@gmail.com>
2017-12-13 21:00:36 +1100

    Day 9 part 2

--- lib/advent/day_9.ex
+++ lib/advent/day_9.ex
@@ -1,55 +1,58 @@
 defmodule Advent.Day9 do
   @doc """
-  iex> Day9.part1("{}")
-  1
+  iex> Day9.parts("{}")
+  {1, 0}
 
-  iex> Day9.part1("{{{}}}")
-  6
+  iex> Day9.parts("{{{}}}")
+  {6, 0}
 
-  iex> Day9.part1("{{},{}}")
-  5
+  iex> Day9.parts("{{},{}}")
+  {5, 0}
 
-  iex> Day9.part1("{{{},{},{{}}}}")
-  16
+  iex> Day9.parts("{{{},{},{{}}}}")
+  {16, 0}
 
-  iex> Day9.part1("{<a>,<a>,<a>,<a>}")
-  1
+  iex> Day9.parts("{<a>,<a>,<a>,<a>}")
+  {1, 4}
 
-  iex> Day9.part1("{{<ab>},{<ab>},{<ab>},{<ab>}}")
-  9
+  iex> Day9.parts("{{<ab>},{<ab>},{<ab>},{<ab>}}")
+  {9, 8}
 
-  iex> Day9.part1("{{<!!>},{<!!>},{<!!>},{<!!>}}")
-  9
+  iex> Day9.parts("{{<!!>},{<!!>},{<!!>},{<!!>}}")
+  {9, 0}
 
-  iex> Day9.part1("{{<a!>},{<a!>},{<a!>},{<ab>}}")
-  3
+  iex> Day9.parts("{{<a!>},{<a!>},{<a!>},{<ab>}}")
+  {3, 17}
   """
-  def part1(input), do: do_part1(String.codepoints(input), {:group, 0}, 0)
+  def parts(input), do: do_parts(String.codepoints(input), {:group, 0}, {0, 0})
 
   # Base case
-  def do_part1([], {:group, 0}, score), do: score
+  def do_parts([], {:group, 0}, results), do: results
 
   # Group terminators
-  def do_part1(["{" | rest], {:group, level}, score) do
-    do_part1(rest, {:group, level+1}, score+level+1)
+  def do_parts(["{" | rest], {:group, level}, {score, cancelled}) do
+    do_parts(rest, {:group, level+1}, {score+level+1, cancelled})
   end
-  def do_part1(["}" | rest], {:group, level}, score) do
-    do_part1(rest, {:group, level-1}, score)
+  def do_parts(["}" | rest], {:group, level}, results) do
+    do_parts(rest, {:group, level-1}, results)
   end
 
   # Garbage terminators
-  def do_part1(["<" | rest], {:group, level}, score) do
-    do_part1(rest, {:garbage, level}, score)
+  def do_parts(["<" | rest], {:group, level}, results) do
+    do_parts(rest, {:garbage, level}, results)
   end
-  def do_part1([">" | rest], {:garbage, level}, score) do
-    do_part1(rest, {:group, level}, score)
+  def do_parts([">" | rest], {:garbage, level}, results) do
+    do_parts(rest, {:group, level}, results)
   end
 
   # Everything else
-  def do_part1(["!", _ | rest], state, score) do
-    do_part1(rest, state, score)
+  def do_parts(["!", _ | rest], state, results) do
+    do_parts(rest, state, results)
   end
-  def do_part1([_ | rest], state, score) do
-    do_part1(rest, state, score)
+  def do_parts([_ | rest], {:group, _}=state, results) do
+    do_parts(rest, state, results)
+  end
+  def do_parts([_ | rest], {:garbage, _}=state, {score, cancelled}) do
+    do_parts(rest, state, {score, cancelled+1})
   end
 end
