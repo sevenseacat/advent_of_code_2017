b53bbc28bf1088b2f64d743768d0e10b76d9643d
Rebecca Skinner <traybaby@gmail.com>
2017-12-17 17:34:05 +1100

    Day 12 part 1

---
+++ test/day_12_test.exs
@@ -0,0 +1,24 @@
+defmodule Advent.Day12Test do
+  use ExUnit.Case
+  alias Advent.Day12
+  doctest Advent.Day12
+
+  test "input parsing" do
+    output = File.read!("test/data/day_12") |> Day12.parse_input
+
+    assert output == %{
+      0 => [2],
+      1 => [1],
+      2 => [0, 3, 4],
+      3 => [2, 4],
+      4 => [2, 3, 6],
+      5 => [6],
+      6 => [4, 5]
+    }
+  end
+
+  test "part 1" do
+    assert Day12.part1(%{0 => [2], 1 => [1], 2 => [0, 3, 4], 3 => [2, 4], 4 => [2, 3, 6], 5 => [6], 6 => [4, 5]}, 0)
+      == [0, 2, 3, 4, 5, 6]
+  end
+end
